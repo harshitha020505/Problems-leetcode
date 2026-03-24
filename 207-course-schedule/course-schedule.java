6d57158a0fa03a225216ca26753310738d9a1d67
Bobbala Harshitha <harshithabobbala392@gmail.com>
2026-03-24 20:04:00 +0530

    Time: 7 ms (60.42%) | Memory: 47 MB (83.92%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+      List<List<Integer>> graph=new ArrayList<>();
+      for(int i=0;i<numCourses;i++){
+        graph.add(new ArrayList<>());
+      }   
+      Queue<Integer> q=new LinkedList<>();
+      int[] inorder=new int[numCourses];
+      for(int[] g:prerequisites){
+        graph.get(g[1]).add(g[0]);
+        inorder[g[0]]++;
+      }
+      for(int i=0;i<numCourses;i++){
+        if(inorder[i]==0){
+            q.add(i);
+        }
+      }
+      int count=0;
+      while(!q.isEmpty()){
+        int a=q.poll();
+        count++;
+        for(int i:graph.get(a)){
+            inorder[i]--;
+            if(inorder[i]==0){
+                // count++;
+                q.add(i);
+            }
+        }
+      }
+      return count==numCourses;
+    }
+}
